50734716c2ecc0be0cf6d521814608f023b41f3a
denis <deadkor26@gmail.com>
2024-01-03 20:21:54 +0200

    task-4-5-6-+style

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+function changeBackgroundColor() {
+  const body = document.querySelector('body');
+  const colorSpan = document.querySelector('.color');
+
+  body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = body.style.backgroundColor;
+}
+
+const changeColorButton = document.querySelector('.change-color');
+changeColorButton.addEventListener('click', changeBackgroundColor);
