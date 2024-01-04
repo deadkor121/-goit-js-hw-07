455959ebc88ce703e863467950f673af4e7020bc
denis <deadkor26@gmail.com>
2024-01-04 20:04:10 +0200

    refacroring -task-1-2-3-4-5

--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,35 @@
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+createButton.addEventListener('click', createBoxes);
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const elementsNumber = Number(input.value);
+
+  if (elementsNumber >= 1 && elementsNumber <= 100) {
+    const boxes = [];
+
+    for (let i = 0; i < elementsNumber; i += 1) {
+      const box = document.createElement('div');
+      box.style.width = `${30 + i * 10}px`;
+      box.style.height = `${30 + i * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      
+      boxes.push(box);
+    }
+
+    boxesDiv.append(...boxes);
+    input.value = '';
   }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
